4b929d7b16650a10ba44aa43ba14345556148a4e
Zdenek Obst <zdenek.obst@gmail.com>
2016-08-13 09:06:37 +0200

    Updated dxcrypto-bc test

--- dxcrypto-bc/src/test/java/cz/d1x/dxcrypto/encrytion/bc/BCEncryptionAlgorithmsTest.java
+++ dxcrypto-bc/src/test/java/cz/d1x/dxcrypto/encrytion/bc/BCEncryptionAlgorithmsTest.java
@@ -6,6 +6,7 @@ import cz.d1x.dxcrypto.encryption.EncryptionEngine;
 import cz.d1x.dxcrypto.encryption.bc.BouncyCastleFactories;
 import cz.d1x.dxcrypto.encryption.crypto.CryptoFactories;
 import cz.d1x.dxcrypto.encryption.key.DerivedKeyParams;
+import cz.d1x.dxcrypto.encryption.key.EncryptionKeyFactory;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -19,35 +20,66 @@ public class BCEncryptionAlgorithmsTest {
     private final Random RANDOM = new Random();
 
     @Test
-    public void aesHaveSameOutputsAsCryptoForStringBasedPasswords() {
+    public void aes128HaveSameOutputsAsCryptoForStringBasedPasswords() {
+        int keySize = 128;
+        int blockSize = 128;
+
+        EncryptionKeyFactory<ByteArray, DerivedKeyParams> cryptoKF = CRYPTO_FACTORIES.derivedKeyFactory();
+        EncryptionKeyFactory<ByteArray, DerivedKeyParams> bcKF = BC_FACTORIES.derivedKeyFactory();
+        ByteArray key = compareTwoKeyFactories(cryptoKF, bcKF, keySize);
+
+        EncryptionEngine cryptoEngine = CRYPTO_FACTORIES.aes().newEngine(key);
+        EncryptionEngine bcEngine = BC_FACTORIES.aes().newEngine(key);
+        byte[] iv = new byte[blockSize / 8];
+        RANDOM.nextBytes(iv);
+        compareTwoEngines(cryptoEngine, bcEngine, iv);
+    }
+
+    @Test
+    public void aes256HaveSameOutputsAsCryptoForStringBasedPasswords() {
+        int keySize = 256;
+        int blockSize = 128;
+
+        EncryptionKeyFactory<ByteArray, DerivedKeyParams> cryptoKF = CRYPTO_FACTORIES.derivedKeyFactory();
+        EncryptionKeyFactory<ByteArray, DerivedKeyParams> bcKF = BC_FACTORIES.derivedKeyFactory();
+        ByteArray key = compareTwoKeyFactories(cryptoKF, bcKF, keySize);
+
+        EncryptionEngine cryptoEngine = CRYPTO_FACTORIES.aes256().newEngine(key);
+        EncryptionEngine bcEngine = BC_FACTORIES.aes256().newEngine(key);
+        byte[] iv = new byte[blockSize / 8];
+        RANDOM.nextBytes(iv);
+        compareTwoEngines(cryptoEngine, bcEngine, iv);
+    }
+
+    private ByteArray compareTwoKeyFactories(EncryptionKeyFactory<ByteArray, DerivedKeyParams> factory1,
+                                             EncryptionKeyFactory<ByteArray, DerivedKeyParams> factory2,
+                                             int keySize) {
         byte[] keyPass = "s3ce3t-keyPass".getBytes(StandardCharsets.UTF_8);
         byte[] salt = new byte[17];
         RANDOM.nextBytes(salt);
         int iterations = 27;
-        DerivedKeyParams keyParams = new DerivedKeyParams(keyPass, salt, iterations, 128);
+        DerivedKeyParams keyParams = new DerivedKeyParams(keyPass, salt, iterations, keySize);
 
-        ByteArray cryptoKey = CRYPTO_FACTORIES.derivedKeyFactory().newKey(keyParams);
-        ByteArray bcKey = BC_FACTORIES.derivedKeyFactory().newKey(keyParams);
+        ByteArray key1 = factory1.newKey(keyParams);
+        ByteArray key2 = factory2.newKey(keyParams);
 
-        Assert.assertArrayEquals(cryptoKey.getValue(), bcKey.getValue());
+        Assert.assertArrayEquals("Keys should equal", key1.getValue(), key2.getValue());
+        return key1; // doesn't matter, they are equal
+    }
 
-        EncryptionEngine cryptoEngine = CRYPTO_FACTORIES.aes().newEngine(cryptoKey);
-        EncryptionEngine bcEngine = BC_FACTORIES.aes().newEngine(cryptoKey);
-
-        byte[] input = new byte[RANDOM.nextInt(500)];
+    private void compareTwoEngines(EncryptionEngine engine1, EncryptionEngine engine2, byte[] initVector) {
+        byte[] input = new byte[RANDOM.nextInt(200)];
         RANDOM.nextBytes(input);
-        byte[] iv = new byte[128 / 8];
-        RANDOM.nextBytes(iv);
 
-        byte[] cryptoOutput = cryptoEngine.encrypt(input, iv);
-        byte[] bcOutput = bcEngine.encrypt(input, iv);
+        byte[] output1 = engine1.encrypt(input, initVector);
+        byte[] output2 = engine2.encrypt(input, initVector);
 
-//        Assert.assertArrayEquals(cryptoOutput, bcOutput);
+        Assert.assertArrayEquals("Encrypted outputs should equal", output1, output2);
 
-        byte[] cryptoBack = cryptoEngine.decrypt(cryptoOutput, iv);
-        byte[] bcBack = bcEngine.decrypt(bcOutput, iv);
+        byte[] output1Back = engine1.decrypt(output1, initVector);
+        byte[] output2Back = engine2.decrypt(output2, initVector);
 
-        Assert.assertArrayEquals(cryptoBack, input);
-        Assert.assertArrayEquals(bcBack, input);
+        Assert.assertArrayEquals("Input and decrypted output should equal", output1Back, input);
+        Assert.assertArrayEquals("Input and decrypted output should equal", output2Back, input);
     }
 }
